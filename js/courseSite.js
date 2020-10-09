7ad6476011e00e64682aaba96e760eb3660247b8
EthanBell49 <etbell@westmont.edu>
2020-10-09 01:21:27 -0700

    first API

--- js/courseSite.js
+++ js/courseSite.js
@@ -1,1 +1,10 @@
 //alert("Hello from courseSite.js");
+function httpGet() {
+    const Http = new XMLHttpRequest();
+    const url = 'https://class-scheduling-api.herokuapp.com/testing';
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+        return Http.responseText;
+    }
+}
