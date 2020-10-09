47a20369e9eb113d142e1b40bc9083c1f8d612f1
Levi Nelson <levnelson@westmont.edu>
2020-10-08 20:33:59 -0700

    added registration view

---
+++ ios/CoursePlanner/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  CoursePlanner
+//
+//  Created by Levi Nelson on 10/8/20.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
