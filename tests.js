b6dea3fcfea1ae6bb2a22806a887a90de5228b00
brijohnson19 <70553053+brijohnson19@users.noreply.github.com>
2020-11-11 11:02:43 -0800

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,23 +1,29 @@
 $(document).ready(function () {
 
-    $("#results").append("<h2>password validation test</h2>")
+    $("#results").append("<h2>password test</h2>")
 
     // Test case 1 with no lowercase 
-    var password = "1234ABCD";
-    var result = validate(password);
-    $("#results").append("<p> test 1 valid password " + password + " = " + result + "</p>")
+    var psw = "1234ABCD";
+    var result = password(psw);
+    $("#results").append("<p> valid password test 1 " + psw + " = " + result + "</p>")
         // Test case 2 with no numbers
-    var password = "ABCDabcd";
-    var result = validate(password);
-    $("#results").append("<p> test 2 valid password " + password + " = " + result + "</p>")
+    var psw = "ABCDabcd";
+    var result = password(psw);
+    $("#results").append("<p> valid password test 2 " + psw + " = " + result + "</p>")
         // Test case 3 with no uppercase
-    var password = "1234abcd";
-    var result = validate(password);
-    $("#results").append("<p> test 3 valid password " + password + " = " + result + "</p>")
+    var psw = "1234abcd";
+    var result = password(psw);
+    $("#results").append("<p> valid password test 3 " + psw + " = " + result + "</p>")
         // Test case 4 with less than 8 characters
-    var phNumber = "123Abc";
-    var result = validate(password);
-    $("#results").append("<p> test 3 valid password " + password + " = " + result + "</p>")
-
-
+    var psw = "123Abc";
+    var result = password(psw);
+    $("#results").append("<p> valid password test 4 " + psw + " = " + result + "</p>")
+     // Test case 5 with just numbers
+     var psw = "12345678";
+     var result = password(psw);
+     $("#results").append("<p> valid password test 5 " + psw + " = " + result + "</p>")
+    // Test case 6 with correct criteria
+    var psw = "123ABCabc";
+    var result = password(psw);
+    $("#results").append("<p> valid password test 5 " + psw + " = " + result + "</p>")
 });
