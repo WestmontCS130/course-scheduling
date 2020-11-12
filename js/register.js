ded4591bc64222107f769307f6c2556343688f68
EthanBell49 <etbell@westmont.edu>
2020-11-11 19:39:03 -0800

    tests

---
+++ js/register.js
@@ -0,0 +1,11 @@
+function phonenumber(inputtxt) {
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (inputtxt.match(phoneno)) {
+        return true;
+    } else {
+        //alert("Not a valid Phone Number");
+        return false;
+    }
+}
+
+module.exports = phonenumber;
