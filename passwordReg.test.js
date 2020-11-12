7bec732eead8f4a043efb0b22b27b4abde07d3a3
justEClaire <70547689+justEClaire@users.noreply.github.com>
2020-11-11 22:32:58 -0800

    passwordReg.test.js

--- passwordReg.test.js
+++ passwordReg.test.js
@@ -1,6 +1,26 @@
 const password = require('./register');
+
 // Test 1 with spaces and no symbols
+test('Test password', () => {
+    expect("psw2").toMatch(/psw2/);
+});
 
 test('Test password', () => {
-    expect(compare(psw1).toBe(true))
-})
+    expect("psw1").toMatch(/psw1/);
+});
+// Test 2 with no spaces and no symbols
+test('Test password', () => {
+    expect("psw1").toMatch(/psw2/);
+});
+// Test 3 with spaces and symbols
+test('Test password', () => {
+    expect(password("abcdabcd")).toBe(false);
+});
+// Test 4 with spaces and symbols and letters
+test('Test password', () => {
+    expect(password("12345678")).toBe(false);
+});
+
+test('Test password', () => {
+    expect(password("ChickenNuggets21")).toBe(true);
+});
