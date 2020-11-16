6fa30a21a34a66615176a15b9e5cd4f264f4052b
Levi Nelson <levnelson@westmont.edu>
2020-11-15 16:48:27 -0800

    Fixed XCTests

--- ios/CoursePlannerTests/CoursePlannerTests.swift
+++ ios/CoursePlannerTests/CoursePlannerTests.swift
@@ -5,28 +5,29 @@
 //  Created by Levi Nelson on 11/15/20.
 //
 
+@testable import CoursePlanner
 import XCTest
 
 class CoursePlannerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testGoodRegistration() {
+            
+            let account = Register(phone: "9492450909", password1: "ChickenPotPie", password2: "ChickenPotPie")
+            
+            XCTAssertTrue(account.isGoodRegistration())
+            
         }
-    }
+        
+        func testDifferentPass() {
+            let account = Register(phone: "9492450909", password1: "ChickenPoePie", password2: "ChickenPotPie")
+            
+            XCTAssertFalse(account.isGoodRegistration())
+        }
+        
+        func testBadPhoneNumber() {
+            let account = Register(phone: "949245", password1: "ChickenPotPie", password2: "ChickenPotPie")
+            
+            XCTAssertFalse(account.isGoodRegistration())
+        }
 
 }
