15112c9a8538158119b495b0501ec329b358f555
justEClaire <70547689+justEClaire@users.noreply.github.com>
2020-11-19 20:15:09 -0800

    Add files via upload

---
+++ viewSchedule.js
@@ -0,0 +1,39 @@
+"use strict";
+
+$(document).ready(function() {
+
+    //alert('Hello View Schedule');
+    //$('.phone_us').mask('000-000-0000');
+
+
+    // Adds a click event to the GetDogPic button
+    $("#viewCS").click(function() {
+        //alert('Working?');
+
+        // This makes the ajax call and fires a success or error function defined inline.
+        $.ajax({
+            url: 'https://class-scheduling-api.herokuapp.com/years',
+            type: 'GET', // POST  PUT  PATCH  DELETE  GET
+            crossDomain: true,
+            dataType: 'json',             
+            beforeSend: function(xhr) {
+                xhr.withCredentials = true;                
+            },
+            success: function(data) {
+                console.table(data[1]);
+                console.table(data[2]);
+
+                // This is the success function event.
+                // Notice the data is passed as a parameter.  It is in json format can you can reference the properties data.message and data.success.
+                // https://dog.ceo/dog-api/ The documentation tells you the layout of the return data.
+                alert('Data: ' + data.stringify);
+
+                //$("#DogPic").attr("src", data.message);
+            },
+            error: function(request, error) {
+                // This function is call if an error occurs.
+                alert("Request: " + JSON.stringify(request));
+            }
+        });
+    });
+});
