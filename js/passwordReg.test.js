a6001278af884f356d5a9d8511e211dd18c38d5e
EthanBell49 <etbell@westmont.edu>
2020-12-07 17:56:57 -0800

    no message

--- js/passwordReg.test.js
+++ js/passwordReg.test.js
@@ -2,15 +2,15 @@ const password = require('./register');
 
 // Test 1 with spaces and no symbols
 test('Test password', () => {
-    expect("psw2").toMatch(/psw2/);
+    expect(password("abcdabcd")).toBe(false);
 });
 
 test('Test password', () => {
-    expect("psw1").toMatch(/psw1/);
+    expect(password("abcdabcd")).toBe(false);
 });
 // Test 2 with no spaces and no symbols
 test('Test password', () => {
-    expect("psw1").toMatch(/psw2/);
+    expect(password("abcdabcd")).toBe(false);
 });
 // Test 3 with spaces and symbols
 test('Test password', () => {
@@ -19,8 +19,4 @@ test('Test password', () => {
 // Test 4 with spaces and symbols and letters
 test('Test password', () => {
     expect(password("12345678")).toBe(false);
-});
-
-test('Test password', () => {
-    expect(password("ChickenNuggets21")).toBe(true);
 });
--- js/viewSchedule.js
+++ js/viewSchedule.js
@@ -2,35 +2,125 @@
 
 $(document).ready(function() {
 
-    //alert('Hello View Schedule');
-    //$('.phone_us').mask('000-000-0000');
+    //Sets up JavaScript AJAX library for pulling data from Heroku API
+    $.ajax({
+        url: 'https://class-scheduling-api.herokuapp.com/years',
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        beforeSend: function(xhr) {
+            xhr.withCredentials = true;
+        },
+        success: function(data) {
+            console.log(data);
 
+            //Sets up table with data from API 
+            var html_to_append = '';
+            html_to_append += '<table>'
+            html_to_append += '<tr>'
+            html_to_append += '<th>Year</th>'
+            html_to_append += '<th>Name</th>'
+            html_to_append += '</tr>'
 
-    // Adds a click event to the GetDogPic button
-    $("#viewCS").click(function() {
-        alert('Working?');
+            //.each visits each element in the data under yearItem and sorts it by idyears and rName1
+            $.each(data, function(i, yearItem) {
 
-        // This makes the ajax call and fires a success or error function defined inline.
-        $.ajax({
-            url: 'https://class-scheduling-api.herokuapp.com/schedule',
-            type: 'GET', // POST  PUT  PATCH  DELETE  GET
-            crossDomain: true,
-            dataType: 'json',             
-            beforeSend: function(xhr) {
-                xhr.withCredentials = true;                
-            },
-            success: function(data) {
-                // This is the success function event.
-                // Notice the data is passed as a parameter.  It is in json format can you can reference the properties data.message and data.success.
-                // https://dog.ceo/dog-api/ The documentation tells you the layout of the return data.
-                alert('Data: ' + data.stringify);
+                console.log(yearItem.idyears);
+                console.log(yearItem.rName1);
 
-                //$("#DogPic").attr("src", data.message);
-            },
-            error: function(request, error) {
-                // This function is call if an error occurs.
-                alert("Request: " + JSON.stringify(request));
-            }
-        });
+                //checks if the yearName is null, if false then log idyears and rName1 into rows and columns 
+                if (yearItem.rName1 != null) {
+                    html_to_append += '<tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName1 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName2 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName2 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName3 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName3 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName4 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName4 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName5 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName5 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName6 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName6 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName7 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName7 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName8 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName8 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+                if (yearItem.rName9 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName9 + '</td>'
+                    html_to_append += '</tr>'
+                }
+                if (yearItem.rName10 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName10 + '</td>'
+                    html_to_append += '</tr>'
+                }
+                if (yearItem.rName11 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName11 + '</td>'
+                    html_to_append += '</tr>'
+                }
+                if (yearItem.rName12 != null) {
+                    html_to_append += '</tr>'
+                    html_to_append += '<td>' + yearItem.idyears + '</td>'
+                    html_to_append += '<td>' + yearItem.rName12 + '</td>'
+                    html_to_append += '</tr>'
+                }
+
+            });
+
+            //appends all data into html_to_append as a sorted table with name and year of class to be taken 
+            html_to_append += '</table>'
+
+            $("#classes-container").html(html_to_append);
+
+        },
+        error: function(request, error) {
+            // This function is call if an error occurs.
+            alert("Request: " + JSON.stringify(request));
+        }
     });
 });
