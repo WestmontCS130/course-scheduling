8f54120472774f7221635362ee66e0ec5a53c84e
brijohnson19 <70553053+brijohnson19@users.noreply.github.com>
2020-12-11 10:52:01 -0800

    Update viewSchedule.js

--- viewSchedule.js
+++ viewSchedule.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
         // This makes the ajax call and fires a success or error function defined inline.
         $.ajax({
-            url: 'https://class-scheduling-api.herokuapp.com/years',
+            url: 'https://class-scheduling-api.herokuapp.com/schedule/1',
             type: 'GET', // POST  PUT  PATCH  DELETE  GET
             crossDomain: true,
             dataType: 'json',             
@@ -20,7 +20,6 @@ $(document).ready(function() {
                 html_to_append += '<table>'
                 html_to_append += '<tr>'
                 html_to_append += '<th>Year</th>'
-                html_to_append += '<th>Name</th>'
                 html_to_append += '</tr>'
 
                 //append data into html as table with years, name
@@ -29,92 +28,34 @@ $(document).ready(function() {
 
                     //visit each element in array, pull out 'yearItem'
 
-                    console.log(yearItem.idyears); 
-                    console.log(yearItem.rName1);
+                    console.log(yearItem.yName);
 
                     //log yearItem - 'id years' (from database)
                     //log yearItem - 'rName1' (from database)
 
-                    if (yearItem.rName1 != null) {
+                    if (yearItem.yName1 != null) {
                     html_to_append += '<tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName1 + '</td>'
+                    html_to_append += '<td>' + yearItem.yName1 + '</td>'
                     html_to_append += '</tr>'
                     }
 
-                    if (yearItem.rName2 != null) {
+                    if (yearItem.yName2 != null) {
                     html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName2 + '</td>'
-                    html_to_append += '</tr>'
-                    }
-
-                    if (yearItem.rName3 != null) {
-                    html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName3 + '</td>'
+                    html_to_append += '<td>' + yearItem.yName2 + '</td>'
                     html_to_append += '</tr>'
                     }
 
-                    if (yearItem.rName4 != null) {
+                    if (yearItem.yName3 != null) {
                     html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName4 + '</td>'
-                    html_to_append += '</tr>'
-                    }
-
-                    if (yearItem.rName5 != null) {
-                    html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName5 + '</td>'
+                    html_to_append += '<td>' + yearItem.yName3 + '</td>'
                     html_to_append += '</tr>'
                     }
 
-                    if (yearItem.rName6 != null) {
+                    if (yearItem.yName4 != null) {
                     html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName6 + '</td>'
-                    html_to_append += '</tr>'
-                    }
-
-                    if (yearItem.rName7 != null) {
-                    html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName7 + '</td>'
+                    html_to_append += '<td>' + yearItem.yName4 + '</td>'
                     html_to_append += '</tr>'
                     }
-
-                    if (yearItem.rName8 != null) {
-                    html_to_append += '</tr>'
-                    html_to_append += '<td>' + yearItem.idyears + '</td>'
-                    html_to_append += '<td>' + yearItem.rName8 + '</td>'
-                    html_to_append += '</tr>'
-                    }
-
-                    if (yearItem.rName9 != null) {
-                        html_to_append += '</tr>'
-                        html_to_append += '<td>' + yearItem.idyears + '</td>'
-                        html_to_append += '<td>' + yearItem.rName9 + '</td>'
-                        html_to_append += '</tr>'
-                    }
-                    if (yearItem.rName10 != null) {
-                        html_to_append += '</tr>'
-                        html_to_append += '<td>' + yearItem.idyears + '</td>'
-                        html_to_append += '<td>' + yearItem.rName10 + '</td>'
-                        html_to_append += '</tr>'
-                    }
-                    if (yearItem.rName11 != null) {
-                        html_to_append += '</tr>'
-                        html_to_append += '<td>' + yearItem.idyears + '</td>'
-                        html_to_append += '<td>' + yearItem.rName11 + '</td>'
-                        html_to_append += '</tr>'
-                    }
-                    if (yearItem.rName12 != null) {
-                        html_to_append += '</tr>'
-                        html_to_append += '<td>' + yearItem.idyears + '</td>'
-                        html_to_append += '<td>' + yearItem.rName12 + '</td>'
-                        html_to_append += '</tr>'
-                        }
 
                 //put new row in table for each year and name(rName1, rName2, rName3...)
                 //takes out 'null' for each one(we gotta fix the formatting of the dataBase)
@@ -132,4 +73,5 @@ $(document).ready(function() {
             }
         });
 });
+
 
