390871cfc7dfd9403a6725eb4356bd46dc8891fd
justEClaire <70547689+justEClaire@users.noreply.github.com>
2020-12-11 12:19:25 -0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,47 @@
+"use strict";
+
+$(document).ready(function() {
+
+    $("#btn-submit").click(function () {
+        var phoneNumber = $('#phoneNumber').val();
+        var password = $('#psw1').val();
+        register(phoneNumber,password);
+    });
+
+
+    function register(phoneNumber, password) {
+
+        // This makes the ajax call and fires a success or error function defined inline.
+        // alert(phoneNumber);
+        // alert(password);
+
+        var userData = {
+            "Phonenumber" : phoneNumber,
+            "Password": password
+        };
+
+        //alert(JSON.stringify(userData));
+
+        $.ajax({
+            type: "POST",
+            url: 'https://class-scheduling-api.herokuapp.com/user',
+            data: JSON.stringify(userData),
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+            window.location.href = "courseSite.html";
+
+                console.log(data);
+                alert("Registration successful! \nPhone Number: " +phoneNumber +"\nPassword: " +password +"\nPlease click OK to continue to the login page")
+            },          
+            error: function(request, error) {
+                console.log("Error:");
+                console.log(error);
+                alert("Registration failed, please check that phone number and password are correct");
+
+    //             alert("Request: " + JSON.stringify(request));
+            }
+        });
+    }
+});
+
